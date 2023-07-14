a676b5367b79fe69599193202ad5cae7ab3fcaed
Joshua Barua <joshuabarua9@hotmail.com>
2023-07-14 12:29:19 +0200

    Get api displaying card data, map data to buttons programatically

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from "react";
 import {Card, CardResponse} from "../types/types";
 
 export default function Home() {
-	const [numberOfCards, setNumberOfCards] = useState(1);
+	const cardSpreadVals = ["3", "6", "10", "12"];
+	const [numberOfCards, setNumberOfCards] = useState<number>();
 	const [tarotCards, setTarotCards] = useState<Card[]>([
 		{
 			type: "",
@@ -18,35 +19,48 @@ export default function Home() {
 	]);
 
 	const fetchCards = async (noOfCards: number) => {
-		const response = await fetch(
-			`https://tarot-api-3hv5.onrender.com/api/v1/cards/random?n=5`
-		);
-		const data = (await response.json()) as CardResponse;
-		setTarotCards(data.cards);
+		if (noOfCards) {
+			try {
+				const response = await fetch(
+					`https://tarot-api-3hv5.onrender.com/api/v1/cards/random?n=${noOfCards}`
+				);
+				const data = (await response.json()) as CardResponse;
+				setTarotCards(data.cards);
+			} catch (e) {
+				console.log("error :>> ", e);
+			}
+		}
+		return;
 	};
 
 	const handleButtonClick = (
 		e: React.MouseEvent<HTMLButtonElement, MouseEvent>
 	) => {
 		const buttonVal = (e.target as HTMLButtonElement).value;
-		if (buttonVal === "3") {
-			setNumberOfCards(parseInt(buttonVal));
-			console.log(numberOfCards);
-		}
+		setNumberOfCards(parseInt(buttonVal));
 	};
 
 	useEffect(() => {
-		fetchCards(numberOfCards).catch((error) => {
-			// console.log("error :>> ", error);
-		});
+		numberOfCards &&
+			fetchCards(numberOfCards).catch((e) => {
+				console.log(e);
+			});
 	}, [numberOfCards]);
 
 	return (
 		<div>
 			<h1>Home</h1>
-			<button onClick={handleButtonClick} name="Three Cards" value={"3"}>
-				Three Cards
-			</button>
+			{cardSpreadVals.map((spreadVal) => {
+				return (
+					<button
+						onClick={handleButtonClick}
+						name={`${spreadVal} Cards`}
+						value={spreadVal}
+					>
+                        { `${ spreadVal }` } Cards
+					</button>
+				);
+			})}
 
 			{tarotCards &&
 				tarotCards.map((card) => {
